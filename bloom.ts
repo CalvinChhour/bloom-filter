877f0776ee3c3ca1225b65a4ce914f62b051e28e
CalvinChhour <calvin.chhour@gmail.com>
2018-11-27 17:53:18 -0700

    Added .gitignore

--- bloom.ts
+++ bloom.ts
@@ -3,27 +3,45 @@ let fs = require("fs")
 let wordList = fs.readFileSync('./wordlist.txt').toString('utf-8')
 /* 
  * Bloom Filter
- * Initially we have an empty array of bits and  a dictionary of words
+ * Initially we have an empty array of m bits, k hashes and a dictionary of words
+ * 
  */
 class bloomFilter {
-    private dict: boolean[]
+    private dictionary: boolean[]
+    private m: number
+    private k: number
 
-    constructor(dict: string[]) {
-        this.dict = new Array(dict.length)
+    constructor(m: number, k: number, dict: string[]) {
+        this.dictionary = new Array(m)
+        this.m = m
+        this.k = k
+
+        for (let i = 0; i < m; i++) {
+            this.dictionary[i] = false
+        }
     }
-    private addWord(word: string): void {
+
+    private addWords(words: string[]): void {
         let hashArray: string[] = Array.from(Md5.hashStr(word).toString())
                 
         hashArray.forEach(e => {
-            this.dict[e] = true
+            this.dictionary[e] = true
         })
     }
 
+    private calculateHash(word: string) {
+
+    }
+
+    public addWord(word: string): void {
+
+    }
+
     public isWord(word: string): boolean {
-        return this.dict[word] !== undefined
+        return this.dictionary[word] !== undefined
     }
 }
 
 let formattedWordList = wordList.split('\n')
-let x = new bloomFilter(formattedWordList)
+let x = new bloomFilter(10, 10, formattedWordList)
 console.log(x.isWord("zine"))
